9c721ca3c59eb94e6add3e39221bce63527b0993
renaudstoffel <stoffelrenaud@gmail.com>
2018-09-21 12:10:52 +0200

    Add Address to Places

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_21_100651) do
+ActiveRecord::Schema.define(version: 2018_09_21_101012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2018_09_21_100651) do
     t.float "distance"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
   create_table "users", force: :cascade do |t|
