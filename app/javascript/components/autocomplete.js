2a997ad9bae159ddd976b920c26eedc756980ca8
renaudstoffel <stoffelrenaud@gmail.com>
2018-09-25 13:35:09 +0200

    Autocomplete

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -1,9 +1,9 @@
 function autocomplete() {
   document.addEventListener("DOMContentLoaded", function() {
-    var placeAddress = document.getElementById('place_address');
+    let placeAddress = document.getElementById('place_address');
 
     if (placeAddress) {
-      var autocomplete = new google.maps.places.Autocomplete(placeAddress, { types: [ 'geocode' ] });
+      let autocomplete = new google.maps.places.Autocomplete(placeAddress, { types: [ 'geocode' ] });
       google.maps.event.addDomListener(placeAddress, 'keydown', function(e) {
         if (e.key === "Enter") {
           e.preventDefault(); // Do not submit the form on Enter.
